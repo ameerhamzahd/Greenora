a0e54e1d48aa5bce11972e5e2c580486e314af2f
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-08-23 04:21:38 +0600

    Greenora MongoDB-Connection done.

---
+++ src/app/lib/mongodb.ts
@@ -0,0 +1,38 @@
+import { MongoClient, MongoClientOptions } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+const options: MongoClientOptions = {};
+
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+// Extend the NodeJS global type to avoid TS errors when using global in dev
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (process.env.NODE_ENV === "development") {
+  // In dev, use a global variable so we don’t create multiple connections
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect().then((client) => {
+      console.log("MongoDB connected (development)");
+      return client;
+    });
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production, always create a new client
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect().then((client) => {
+    console.log("MongoDB connected (production)");
+    return client;
+  });
+}
+
+export default clientPromise;
